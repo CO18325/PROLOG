346cbf28d228eba8538ccdb1948f030114157e5f
Inderpreet Singh <inderpreet221099@gmail.com>
2020-09-23 01:25:16 +0530

    MADE CHANGES TO TOWER OF HANOI PROBLEM

--- TOWER_OF_HANOI/tower_of_hanoi.pl
+++ TOWER_OF_HANOI/tower_of_hanoi.pl
@@ -1,3 +1,20 @@
+/*
+    PROLOG PROJECT
+    TOPIC : TOWER OF HANOI
+    BY : INDERPREET SINGH (C018325)
+*/
+
+
+
+/* 
+    main FUNCTION
+    CALLED BY THE USER 
+    USING THE SWI-PROLOG TERMINAL
+    DISPLAYS THE ENTRY MESSAGE
+    PASSES THE PARAMETERS GIVEN BY USER
+    TO THE MOVE FUNCTION
+*/
+
 main(N,X,Y,Z) :-
     nl,
     nl,
@@ -9,16 +26,39 @@ main(N,X,Y,Z) :-
     nl,
     nl,
     nl,
+    /* CALL THE MOVE FUNCTION */
+    /*
+        PARAMETERS PASSED ARE :
+            N : NO. OF DISKS IN THE INTIAL STATE / PEG
+            X : INTITAL STATE / PEG 
+            Y : NTERMEDIATE STATE / PEG
+            Z : FINAL STATE / PEG 
+    */
     move(N,X,Y,Z).
 
 
+/*
+    move FUNCTION WHEN ONLY ONE DISK IS LEFT IN THE PEG
+    X : STATE IN WHICH THE DISK IS LEFT
+    Y : STATE WHERE THE DISK NEEDS TO BE PLACED
+*/
+
 move(1,X,Y,_) :-
-    write('Move the TOP disk from '),
+    write('MOVE THE TOP DISK FROM:'),
+    nl,
     write(X),
-    write(' to '),
+    nl,
+    write('PLACE IT AT'),
+    nl,
     write(Y),
     nl,
     nl.
+
+
+/*
+    move FUNCTION WHEN FOR N DISKS
+    IT IS RECURSIVE FUNCTION
+*/
 
 move(N,X,Y,Z) :-
     N>1,
